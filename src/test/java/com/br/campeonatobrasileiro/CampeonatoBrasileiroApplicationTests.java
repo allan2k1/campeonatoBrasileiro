4853b2212eca4fae8100b504a9ea8c87673c8c35
Allan Tavares <allantavares@cofcointernational.com>
2024-02-06 20:34:21 -0300

    Initial commit

---
+++ src/test/java/com/br/campeonatobrasileiro/CampeonatoBrasileiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.campeonatobrasileiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatoBrasileiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
